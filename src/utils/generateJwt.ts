8a155e16b3ab039500648275905125791cbb7807
Yusuf Arslan <yusufarsln98@outlook.com>
2024-04-02 00:18:52 +0300

    chore: token expire time is incremented

--- src/utils/generateJwt.ts
+++ src/utils/generateJwt.ts
@@ -7,5 +7,5 @@ const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
 export const generateJwt = (user: User) => {
   const payload = { username: user.username, role: user.role };
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: '30m' }); // Set expiration to 30 minutes
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: `${360 * 24 * 60}` }); // Set expiration to 360 days
 };
