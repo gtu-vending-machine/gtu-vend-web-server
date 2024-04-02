c11eb59b51bcd0fd19b9978af0911f52f3a9c6bf
Yusuf Arslan <yusufarsln98@outlook.com>
2024-04-02 16:40:03 +0300

    fix: early expire bug fixed

--- src/utils/generateJwt.ts
+++ src/utils/generateJwt.ts
@@ -7,5 +7,6 @@ const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
 export const generateJwt = (user: User) => {
   const payload = { username: user.username, role: user.role };
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: `${360 * 24 * 60}` }); // Set expiration to 360 days
+  // Set expiration to 360 days
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: `${360 * 24 * 60}m` });
 };
