604645e471039b11d3e0fc23d83efdbb7a10154c
Yusuf Arslan <yusufarsln98@outlook.com>
2024-04-07 16:36:56 +0300

    feat: adding balance to user

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { prisma } from '../../prismaClient';
 import { UserResponse } from '../../interfaces/User';
+import ErrorResponse from '../../interfaces/ErrorResponse';
 
 const usersRouter = express.Router();
 
@@ -44,6 +45,46 @@ usersRouter.get<{ id: string }, UserResponse | null>(
         .then((user) => {
           res.json(user);
         });
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  },
+);
+
+// add balance to user by id and amount in body
+usersRouter.post<{ id: string }, Omit<UserResponse, 'role'> | ErrorResponse>(
+  '/:id/addBalance',
+  async (req, res, next) => {
+    const { id } = req.params;
+    const { amount } = req.body;
+
+    try {
+      // get users current balance
+      const user = await prisma.user.findUnique({
+        where: { id: Number(id) },
+        select: {
+          balance: true,
+        },
+      });
+
+      // if user does not exist, throw an error
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      const updatedUser = await prisma.user.update({
+        where: { id: Number(id) },
+        data: { balance: user.balance + amount },
+        select: {
+          id: true,
+          name: true,
+          username: true,
+          balance: true,
+        },
+      });
+
+      res.json(updatedUser);
     } catch (error) {
       console.error(error);
       next(error);
